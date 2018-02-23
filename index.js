d5f5a1e44f9fbfe5a5576c7a770a4d5883e45450
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-02-23 11:41:50 -0500

    Add all things needed for running on Heroku with sample stat card

---
+++ index.js
@@ -0,0 +1,39 @@
+//Getting all dependencies
+var express = require('express.io');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.http().io()
+
+//Setting up the port to listen to
+app.set('port', (process.env.PORT || 5000));
+
+//Setting up the resource directory
+app.use(express.static(__dirname + '/public'));
+
+app.use( bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.json());
+
+//Setting up cookie use
+app.use(cookieParser());
+
+//Setting up session handling
+app.use(session({secret: 'this_is_not_secret_actually_but_right_now_it_doesnt_matter'}));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
